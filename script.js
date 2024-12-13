14b764e42ae621158e3ff6cc82c4e1f3f5abe33d
Degonatta <153090059+Degonatta@users.noreply.github.com>
2024-12-13 20:55:16 +0100

    snyggat till kod

--- script.js
+++
@@ -1,5 +0,0 @@
-En array med bokstäver som är tilldelad en bestämd siffra, ec A=1, B=2 etc
-Lagda kort med siffror på framsidan som matchas mot array för att sedan visa rätt bokstav på baksidan
-Ruta under korten där rätt bokstav ska fyllas i.
-Kort vänder på sig om rätt bokstav fylls i. 
-Om fel bokstav fylls i, blinkar bilden till i rött. 
