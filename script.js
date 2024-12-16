d135ac78163befb7735cbcf4161086baf04fbb95
Degonatta <153090059+Degonatta@users.noreply.github.com>
2024-12-16 11:47:57 +0100

    ändring

---
+++ script.js
@@ -0,0 +1,12 @@
+Array som parar bokstav mot siffra
+
+API som slumpar fram ett ord för barn i åldrar 3-7. 4-6 bokstäver.API
+
+Function som omvandlar ordet till siffror och lägger dom i enskilda rutor. 
+
+Function som ger knapparna ett id med bokstav och siffra 
+
+Function som jämför siffrorna från det omvandlade ordet med id på knappen.
+   If match - rutan ändras från siffra till korrekt bokstav
+
+Function som gör att om man trycker på rätt bokstav så blinkar det till i grönt och trycker man fel så blinkar det rött. 
