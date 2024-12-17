eb21ce75cd38e93c6da6ece3e18b4d3fda0e6cd8
Degonatta <153090059+Degonatta@users.noreply.github.com>
2024-12-17 11:34:49 +0100

    cluetwo

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-Array som parar bokstav mot siffra
+function hideStone() {
+   const stoneButton = document.querySelector('.stone-button');
+   stoneButton.style.display = 'none'; // Göm stenen
+}
 
-API som slumpar fram ett ord för barn i åldrar 3-7. 4-6 bokstäver.API
 
-Function som omvandlar ordet till siffror och lägger dom i enskilda rutor. 
 
-Function som ger knapparna ett id med bokstav och siffra 
+//Array som parar bokstav mot siffra
 
-Function som jämför siffrorna från det omvandlade ordet med id på knappen.
-   If match - rutan ändras från siffra till korrekt bokstav
+//API som slumpar fram ett ord för barn i åldrar 3-7. 4-6 bokstäver.API
 
-Function som gör att om man trycker på rätt bokstav så blinkar det till i grönt och trycker man fel så blinkar det rött. 
+//Function som omvandlar ordet till siffror och lägger dom i enskilda rutor. 
+
+//Function som ger knapparna ett id med bokstav och siffra 
+
+//Function som jämför siffrorna från det omvandlade ordet med id på knappen.
+  // If match - rutan ändras från siffra till korrekt bokstav
+
+//Function som gör att om man trycker på rätt bokstav så blinkar det till i grönt och trycker man fel så blinkar det rött.
